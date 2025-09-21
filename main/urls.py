12961d7f67622f85d414e6d974016b827b6557aa
Zaynobiddin <zaynobiddinshaxobiddinov9999@gmail.com>
2025-09-21 15:14:15 +0500

    add url

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
+    path('', views.main, name='main'),
     path('contact', views.contact, name = 'contact'),
     path('about', views.about, name = 'about'),
     path('projects', views.projects, name = 'projects'),
